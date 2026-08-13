ba1f1cedb01a9b7fdf77fcfdd97aa839671349b8
Mariam Fahim <mariamfahim2008@gmail.com>
2026-08-13 18:24:54 +0300

    Fixing again the node mailer

--- notification_service/util/mailer.js
+++ notification_service/util/mailer.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const transporter=nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port:(Number(process.env.SMTP_PORT)),
-    secure: process.env.SMTP_SECURE === 'false',
+    secure: process.env.SMTP_SECURE === 'true',
     requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
