d1d58bc74ad0aed30dda9d02c9b95790f7621815
Mariam Fahim <mariamfahim2008@gmail.com>
2026-08-27 22:16:01 +0300

    Final trial 2

--- backend/util/logger.js
+++ backend/util/logger.js
@@ -1,5 +1,9 @@
 function log(level, message, meta = {}){
     const fn = level === 'error' ? console.error : level === 'warn' ? console.warn : console.log;
-    fn(JSON.stringify({level, timestamp: new Date().toISOString(), message, ...meta}));
+    
+    const cairoTime= new Date(Date.now() + 3 * 60 * 60 * 1000)
+        .toISOString()
+        .replace('Z', '+03:00')
+    fn(JSON.stringify({level, timestamp: cairoTime, message, ...meta}));
 }
 module.exports={log}
